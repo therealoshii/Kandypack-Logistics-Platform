804c2bf89ec8670a53846924840dd96ec0791d3b
therealoshii <vithanageoshan@gmail.com>
2026-09-24 14:55:54 +0530

    Added ELSE in case of neither Driver or Assitant was chosen

--- database/functions/fn_calculate_weekly_hours.sql
+++ database/functions/fn_calculate_weekly_hours.sql
@@ -29,6 +29,11 @@ CREATE FUNCTION fn_calculate_weekly_hours (PersonType VARCHAR(20), PersonID INT,
             INNER JOIN Route r ON tt.RouteID = r.RouteID
             WHERE tt.AssistantID = PersonID
                 AND YEARWEEK(tt.TripDate, 1) = YEARWEEK(WeekDate, 1);
+
+        ELSE
+            SIGNAL SQLSTATE '45000'
+            SET MESSAGE_TEXT = 'Validation Error: Invalid PersonType. Must be DRIVER or ASSISTANT.';
+
         END IF;
         
         RETURN total_hours;
